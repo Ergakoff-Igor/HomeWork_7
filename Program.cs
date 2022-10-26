d3d90036eeee8c813a79d991dcb13afd37def4de
Igor <i.ergakoff@mail.ru>
2022-10-26 20:58:56 +0300

    Решение задания № 50

--- Program.cs
+++ Program.cs
@@ -6,13 +6,39 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 *************************************************************************************************/
 
-Console.WriteLine("Ведите количество строк массива");
+// Console.WriteLine("Ведите количество строк массива");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Ведите количество столбцов массива");
+// int columns = int.Parse(Console.ReadLine()!);
+
+// double[,] array = FillArray(rows, columns, -10, 10);
+// PrintArrayD(array);
+
+/*************************************************************************************************
+Задача 50. 
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
+значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+i = 4, j = 2 -> такого числа в массиве нет
+i = 1, j = 2 -> 2
+*************************************************************************************************/
+
+Console.Write("Ведите количество строк массива: ");
 int rows = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Ведите количество столбцов массива");
+Console.Write("Ведите количество столбцов массива: ");
 int columns = int.Parse(Console.ReadLine()!);
 
-double[,] array = FillArray(rows, columns, -10 ,10);
-PrintArrayD(array);
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+Console.Write("Ведите номер позиции в строке: ");
+int IndexI = int.Parse(Console.ReadLine()!);
+Console.Write("Ведите номер позиции в столбце: ");
+int IndexJ = int.Parse(Console.ReadLine()!);
+OutputIndex(array, IndexI, IndexJ);
 
 
 
@@ -28,10 +54,33 @@ double[,] FillArray(int m, int n, int minValue, int maxValue)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] =new Random().NextDouble() * (maxValue-minValue) + minValue;
+            result[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
         }
     }
     return result;
+}
+// Метод нахождения элемента двумерного массива:
+void OutputIndex(int[,] arr, int roWIndex, int colIndex)
+{
+    if (roWIndex > arr.GetLength(0) || colIndex > arr.GetLength(1))
+    {
+        Console.WriteLine($"Позиции [{roWIndex}, {colIndex}] нет в массиве");
+    }
+    else
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (roWIndex == i && colIndex == j)
+                {
+                    Console.WriteLine($"Элемент, находящийся на позиции [{roWIndex}, {colIndex}] - {arr[i, j]}");
+                    break;
+                }
+
+            }
+        }
+    }
 }
 // Заполнение массива:
 int[,] GetArray(int m, int n, int minValue, int maxValue)
@@ -46,7 +95,19 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     }
     return result;
 }
-// Вывод массива с вещественными числами:
+// Вывод двумерного массива:
+void PrintArray(int[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Console.Write($"{Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+// Вывод двумерного массива с вещественными числами:
 void PrintArrayD(double[,] Array)
 {
     for (int i = 0; i < Array.GetLength(0); i++)
